c86352c4cce3451096b4cdc6d9296a9c253472c0
Janna02 <dendy2@yandex.ru>
2022-10-20 09:58:28 +0300

    tasks 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,51 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел");
+Console.Write("M = ");
+int[] arr = new int[int.Parse(Console.ReadLine())];
+
+int[] ReadNumbers(int[] array)
+{
+    Console.WriteLine("Введите числа: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return new int[] { };
+}
+
+int CountNumbersMoreThanZero(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+}
+
+int[] arrayWittNumbers = ReadNumbers(arr);
+int count = CountNumbersMoreThanZero(arr);
+Console.WriteLine($"Количество чисел больше нуля среди введенных :");
+PrintArray(arr);
+Console.WriteLine($" -> {count}");
+
+
+
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите k1 из уравнения первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2 из уравнения второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b1 из уравнения первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2 из уравнения второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double[] FindPointOfIntersection(double kp1, double kp2, double bp1, double bp2)
+{
+    double x = -(bp2 - bp1) / (kp2 - kp1);
+    double y = kp1 * x + bp1;
+    return new double[] { x, y };
+}
+
+void PrintArrayDouble(double[] array)
+{
+    Console.Write("(");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine(")");
+}
+
+double[] arr = FindPointOfIntersection(k1, k2, b1, b2);
+Console.WriteLine($"Точка пересечения двух прямых:");
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
+PrintArrayDouble(arr);
+
+
+
